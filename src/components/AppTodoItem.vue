7392fe1894cfc86edcce32924e8334716a56aa0a
Aleksey Konyakhin <konyaxin.1998@gmail.com>
2025-04-10 18:26:48 +0300

    Feat: Added completion and deletion of todo items

--- src/components/AppTodoItem.vue
+++ src/components/AppTodoItem.vue
@@ -1,21 +1,36 @@
 <template>
-    <li class="todo-item todo-item--done">
+    <li class="todo-item" :class="{ 'todo-item--done': todo.completed }" @click="toggleTodo">
         <div class="todo-item__status">
             <i class="bi bi-check2"></i>
         </div>
-        <span class="todo-item__text">Learn the basics of Vue</span>
-        <button class="todo-item__remove-button">
+        <span class="todo-item__text">{{ todo.text }}</span>
+        <button class="todo-item__remove-button" @click.stop="deleteTodo">
             <i class="bi bi-trash3"></i>
         </button>
     </li>
 </template>
 
 <script setup lang="ts">
+// import { computed } from 'vue'
+import type { Todo } from '../interfaces/Todos'
+
 interface Props {
-    word: string
+    todo: Todo
 }
 
-defineProps<Props>()
+const props = defineProps<Props>()
+const emit = defineEmits<{
+    (e: 'toggleTodo', id: number): void
+    (e: 'deleteTodo', id: number): void
+}>()
+
+const toggleTodo = () => {
+    emit('toggleTodo', props.todo.id)
+}
+
+const deleteTodo = () => {
+    emit('deleteTodo', props.todo.id)
+}
 </script>
 
 <style scoped></style>
--- src/components/AppTodoList.vue
+++ src/components/AppTodoList.vue
@@ -1,14 +1,39 @@
 <template>
     <ul class="todo-list">
-        <AppTodoItem :word="test" />
+        <!-- <AppTodoItem v-for="todo in todos" :todo="{ id: todo.id, text: todo.text, completed: todo.completed }" /> -->
+        <AppTodoItem
+            @delete-todo="deleteTodo"
+            @toggle-todo="toggleTodo"
+            v-for="todo in todos"
+            :key="todo.id"
+            :todo="todo"
+        />
     </ul>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 import AppTodoItem from './AppTodoItem.vue'
+import type { Todo } from '../interfaces/Todos'
 
-const test = ref('f')
+const todos = ref<Todo[]>([
+    { id: 0, text: 'Learn the basics of Vue', completed: true },
+    { id: 1, text: 'Learn the basics of Typescript', completed: false },
+    { id: 2, text: 'Subscribe to the channel', completed: false }
+])
+
+const toggleTodo = (id: number) => {
+    const targetTodo = todos.value.find((item: Todo) => item.id === id)
+
+    if (targetTodo) {
+        targetTodo.completed = !targetTodo.completed
+    }
+}
+
+const deleteTodo = (id: number) => {
+    const newTodos = todos.value.filter((item: Todo) => item.id !== id)
+    todos.value = newTodos
+}
 </script>
 
 <style scoped></style>
